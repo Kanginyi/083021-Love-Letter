6baec82b080e89c4b8fe07d61f891c921e02bd9e
Eric Yi <erickanginyi@gmail.com>
2021-11-22 18:55:06 -0500

    Update initial routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,19 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
 
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
 function App() {
   return (
     <>
-      <Navbar />
-      <div id="main-content">
-        <h1>fuck you</h1>
-      </div>
+      <Router>
+        <Navbar />
+        <div id="main-content">
+          <Routes>
+            <h1>fuck you</h1>
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
