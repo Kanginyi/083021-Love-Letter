7f509bbbd1840a39bef97087a20d8d7fba37b3ee
Eric Yi <erickanginyi@gmail.com>
2021-11-23 18:58:23 -0500

    Update with spotify playlist

--- client/src/Components/Playlist.js
+++ client/src/Components/Playlist.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import "../Styling/Playlist.css"
 
 function Playlist() {
     return (
-        <h1>asd</h1>
+        <>
+            <div>
+                <h1>Cohort Playlist</h1>
+                <iframe
+                    src="https://open.spotify.com/embed/playlist/37i9dQZF1DX8Uebhn9wzrS?utm_source=generator"
+                    width="100%"
+                    height="380"
+                    frameBorder="0"
+                    allowfullscreen=""
+                    allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture">
+                </iframe>
+            </div>
+        </>
     );
 }
+
+// Create 3 playlists, one for section a, b, and total
 
 export default Playlist;
