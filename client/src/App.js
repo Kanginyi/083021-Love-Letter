cca7f96fa62f51cb2afef35af2aa07f072a9b467
Eric Yi <erickanginyi@gmail.com>
2021-11-24 02:50:54 -0500

    Add route to Cohort.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
 import Homepage from "./Components/Homepage";
+import Cohort from "./Components/Cohort";
 import Playlist from "./Components/Playlist";
 import Locations from "./Components/Locations";
 import Options from "./Components/Options";
@@ -15,6 +16,7 @@ function App() {
         <div id="main-content">
           <Routes>
             <Route path="/" element={<Homepage/>}/>
+            <Route path="cohort" element={<Cohort/>}/>
             <Route path="playlist" element={<Playlist/>}/>
             <Route path="locations" element={<Locations/>}/>
             <Route path="options" element={<Options/>}/>
