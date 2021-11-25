c74a195499e8fbaf4d289d9b698d755e2457b470
Eric Yi <erickanginyi@gmail.com>
2021-11-25 02:00:22 -0500

    Migrate tables

---
+++ db/schema.rb
@@ -0,0 +1,55 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_11_24_230351) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "instructors", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.string "description"
+    t.string "song"
+    t.string "current_location"
+    t.string "location_from"
+    t.string "interesting_fact"
+    t.string "hobbies"
+    t.string "picture"
+    t.string "favorite_food"
+    t.string "github"
+    t.float "years_worked"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.string "description"
+    t.string "song"
+    t.string "current_location"
+    t.string "location_from"
+    t.string "interesting_fact"
+    t.string "hobbies"
+    t.string "picture"
+    t.string "favorite_food"
+    t.string "github"
+    t.float "flatiron_rating"
+    t.bigint "instructor_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["instructor_id"], name: "index_students_on_instructor_id"
+  end
+
+  add_foreign_key "students", "instructors"
+end
