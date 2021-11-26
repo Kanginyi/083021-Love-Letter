d0ee58d88fc2aa95c2601f370d7f6c87a45d3449
Eric Yi <erickanginyi@gmail.com>
2021-11-26 07:36:49 -0500

    Import child components

--- client/src/Components/Cohort.js
+++ client/src/Components/Cohort.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import "../Styling/Cohort.css"
 
+import CohortCarousel from './CohortCarousel';
+import CohortInfoBox from './CohortInfoBox';
+import CohortInfoCircle from './CohortInfoCircle';
+
 function Cohort() {
+    const [personInfo, setPersonInfo] = useState([]);
+
+    useEffect(() => {
+        fetch("/students")
+            .then(resp => resp.json())
+            .then(data => setPersonInfo(data));
+    }, []);
+
     return (
-        <h1>Cohort pagexd</h1>
+        <>
+            <h1>wtf</h1>
+            <CohortCarousel personInfo={personInfo}/>
+            <CohortInfoBox />
+            <CohortInfoCircle personInfo={personInfo}/>
+        </>
     );
 }
 
