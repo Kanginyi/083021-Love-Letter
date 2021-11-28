7966e37a6da4f40c1eeb28284e9fccb12baf9c20
Eric Yi <erickanginyi@gmail.com>
2021-11-27 20:57:45 -0500

    Create helper file

---
+++ client/src/Components/CohortCloseCircleItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function CohortCloseCircleItem() {
+    return ( 
+        <div className="close-circular-menu">
+            <div></div>
+            <div></div>
+        </div>
+    );
+}
+
+export default CohortCloseCircleItem;
---
+++ client/src/Components/CohortOpenCircleItem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function CohortOpenCircleItem() {
+    return (
+        <div className="circular-menu-icon">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    );
+}
+
+export default CohortOpenCircleItem;
