4435b8dc6cc30ca64163e1dd4e51dc8c442418d9
Eric Yi <erickanginyi@gmail.com>
2021-11-29 01:28:54 -0500

    Create find function and implement passed in props data

--- client/src/Components/CohortInfoBox.js
+++ client/src/Components/CohortInfoBox.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import "../Styling/CohortInfoBox.css"
 
-function CohortInfoBox({personInfo}) {
-    console.log(personInfo);
+function CohortInfoBox({personInfo, clickedName}) {
+    const choosePerson = personInfo?.find(person => person.name === clickedName)
 
     return (
         <div className="card">
-            <div className="card-header">{personInfo[4]?.name}</div>
+            <div className="card-header">{choosePerson?.name}</div>
             <div className="card-body">
                 <img
-                    src={personInfo[4]?.picture}
-                    alt={personInfo[4]?.name}
-                    title={personInfo[4]?.name}
+                    src={choosePerson?.picture}
+                    alt={choosePerson?.name}
+                    title={choosePerson?.name}
                     width="100%"
                 /> <br/>
-                {personInfo[4]?.description}
+                {choosePerson?.description}
             </div>
             <div className="card-footer">
-                {personInfo[4]?.location_from}
+                {choosePerson?.location_from}
             </div>
         </div>
     );
@@ -25,21 +25,9 @@ function CohortInfoBox({personInfo}) {
 
 export default CohortInfoBox;
 
+// HEY HEY UPDATE THE THING SO THAT THERE'S ACTUALLY SOMETHING THAT'S THERE ON PAGE LOAD
+
 //Load these inside of the fill in bbox on the side
 
 // description: "An idiot trying to learn how to not be an idiot."
 // location_from: "New York City, New York"
-
-// age: 26
-// current_location: "New York"
-// description: "An idiot trying to learn how to not be an idiot."
-// favorite_food: "Sushi, Korean BBQ, Noodles"
-// flatiron_rating: 9
-// github: "https://github.com/Kanginyi"
-// hobbies: "Video games, reading, walking around with my cat, "
-// id: 5
-// instructor: {id: 1, name: 'Dickhead McGee'}
-// interesting_fact: "I have a polydactyl cat named Henri who has 7 fingers on both of his front two paws!"
-// location_from: "New York City, New York"
-// picture: "https://i.imgur.com/T3Dnjf6.jpg"
-// song: "<iframe src=\"https://open.spotify.com/embed/track/51Ful1v7b81FchIFFTAQ05
