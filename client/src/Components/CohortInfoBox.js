c1b5d17ca8b3a9848109dda223b6735463525842
Eric Yi <erickanginyi@gmail.com>
2021-11-29 13:25:49 -0500

    Add placeholder data and update dynamic data

--- client/src/Components/CohortInfoBox.js
+++ client/src/Components/CohortInfoBox.js
@@ -1,31 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "../Styling/CohortInfoBox.css"
 
 function CohortInfoBox({personInfo, clickedName}) {
     const choosePerson = personInfo?.find(person => person.name === clickedName)
 
+
+    // HEY HEY UPDATE THE THING SO THAT THERE'S ACTUALLY SOMETHING THAT'S THERE ON PAGE LOAD
+
+    // Bing usestate inside of this shit and then set it equal to a empty string
+    // if it's that, conditional render each of the component things inside the card
+    // set the initial states to the empy things
+
+    // Ribbon thing on youtube
+
     return (
         <div className="card">
-            <div className="card-header">{choosePerson?.name}</div>
+            <div className="card-header">{clickedName !== "" ? choosePerson?.name : "Bingus Bongus"}</div>
             <div className="card-body">
                 <img
-                    src={choosePerson?.picture}
+                    className="card-image"
+                    src={clickedName !== "" ? choosePerson?.picture : "https://i.imgur.com/gaxXG7T.jpg"}
                     alt={choosePerson?.name}
                     title={choosePerson?.name}
                     width="100%"
                 /> <br/>
-                {choosePerson?.description}
+                {clickedName !== "" ? choosePerson?.description : "Shiet"}
             </div>
             <div className="card-footer">
-                {choosePerson?.location_from}
+                From: {clickedName !== "" ? choosePerson?.location_from : "???"}
             </div>
         </div>
     );
 }
 
 export default CohortInfoBox;
-
-// HEY HEY UPDATE THE THING SO THAT THERE'S ACTUALLY SOMETHING THAT'S THERE ON PAGE LOAD
 
 //Load these inside of the fill in bbox on the side
 
