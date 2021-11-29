a88434f3ddceed444845feda88f997882d6a566c
Eric Yi <erickanginyi@gmail.com>
2021-11-29 13:26:26 -0500

    Create new playlist and update code

--- client/src/Components/Playlist.js
+++ client/src/Components/Playlist.js
@@ -7,13 +7,14 @@ function Playlist() {
             <div>
                 <h1>Cohort Playlist</h1>
                 <iframe
-                    title="wtf"
-                    src="https://open.spotify.com/embed/playlist/37i9dQZF1DX8Uebhn9wzrS?utm_source=generator"
+                    title="Cohort Playlist"
+                    src="https://open.spotify.com/embed/playlist/4mUmJlqIADouHmmh0NOuqY?utm_source=generator"
                     width="100%"
                     height="380"
                     frameBorder="0"
                     allowfullscreen=""
-                    allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture">
+                    allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                >
                 </iframe>
             </div>
         </>
