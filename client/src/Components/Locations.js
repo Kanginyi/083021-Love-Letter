21e45eea84ab42bc44156f8be139b27091d724d1
Eric Yi <erickanginyi@gmail.com>
2021-11-29 22:10:33 -0500

    Update page layout

--- client/src/Components/Locations.js
+++ client/src/Components/Locations.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import LocationsPeople from './LocationsPeople';
 import "../Styling/Locations.css"
 
-function Locations() {
+function Locations({personInfo}) {
+    const peopleBubbles = personInfo?.map(person => {
+        return <LocationsPeople key={person.name} picture={person.picture} name={person.name} location={person.current_location}/>
+    })
+
     return (
         <>
         <div id="united-state-map-container">
+            {peopleBubbles}
             <img
                 id="united-states-map"
                 src="https://i.imgur.com/GGFrGLr.png"
@@ -16,3 +22,10 @@ function Locations() {
 }
 
 export default Locations;
+
+{/* <img
+id={nameID}
+className={`locations-people ${location}`}
+src={picture}
+alt={name}
+/> */}
