3cbfead909747cbfe5372849425a2b98376314dc
Eric Yi <erickanginyi@gmail.com>
2021-11-29 22:06:04 -0500

    Remove props functions

--- client/src/Components/Cohort.js
+++ client/src/Components/Cohort.js
@@ -1,24 +1,11 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import "../Styling/Cohort.css"
 
 import CohortCarousel from './CohortCarousel';
 import CohortInfoBox from './CohortInfoBox';
 import CohortInfoCircle from './CohortInfoCircle';
 
-function Cohort() {
-    const [personInfo, setPersonInfo] = useState([]);
-    const [clickedName, setClickedName] = useState("");
-
-    useEffect(() => {
-        fetch("/students")
-            .then(resp => resp.json())
-            .then(data => setPersonInfo(data));
-    }, []);
-
-    const handleImageClick = (name) => {
-        setClickedName(name);
-    }
-
+function Cohort({personInfo, handleImageClick, clickedName}) {
     return (
         <>
             <h1>wtf</h1>
