7a932dc00e02cae28cecd441f04d10bf8c745877
Eric Yi <erickanginyi@gmail.com>
2021-12-02 18:16:43 -0500

    bing bong

--- client/src/Components/CohortInfoCircle.js
+++ client/src/Components/CohortInfoCircle.js
@@ -6,6 +6,14 @@ import "../Styling/CohortInfoCircle.css"
 
 // *THIS IS A TEST, DOUNLE CHECK THIS SHIT LATER
 import ModalGithub from './ModalGithub';
+import ModalFood from './ModalFood';
+import ModalHobbies from './ModalHobbies';
+import ModalLocation from './ModalLocation';
+import ModalAge from './ModalAge';
+import ModalRating from './ModalRating';
+import ModalFact from './ModalFact';
+import ModalSpotify from './ModalSpotify';
+
 
 import {AiFillGithub, AiOutlineNumber, AiFillStar} from "react-icons/ai";
 import {FaSpotify, FaQuestionCircle} from "react-icons/fa";
@@ -13,47 +21,77 @@ import {IoFastFoodOutline} from "react-icons/io5";
 import {MdDescription} from "react-icons/md";
 import {GoLocation} from "react-icons/go"
 
-function CohortInfoCircle({personInfo}) {
-    // const renderPerson = personInfo?.map(person => {
-    //     return <CohortPerson key={person.id} person={person}/>
-    // });
+function CohortInfoCircle({personInfo, clickedName}) {
+    const choosePerson = personInfo?.find(person => person.name === clickedName)
 
+    // This is going to be the information that's passed in through for each person
+    const personGithub = choosePerson?.github;
+    const personFood = choosePerson?.favorite_food;
+    const personHobbies = choosePerson?.hobbies;
+    const personLocation = choosePerson?.current_location;
+    const personAge = choosePerson?.age;
+    const personRating = choosePerson?.flatiron_rating;
+    const personFact = choosePerson?.interesting_fact;
+    const personSpotify = choosePerson?.song;
+
+    // Is the modal open or not?
     const [isOpen, setIsOpen] = useState(false);
 
     const handleSetIsOpen = () => {
         setIsOpen(prevValue => !prevValue);
     }
 
-    // Create states for each of the individuals modals and fuck around with that shit yout ustpid fuck
+    // Click this to open the modal
     const [openModal, setOpenModal] = useState(false);
 
-    const handleSetOpenModal = () => {
+    // const [selectedModal, setSelectedModal] = ("");
+
+    const handleSetOpenModal = e => {
+        // Switch to check which modal to open up
+        // switch (e.target.id) {
+        //     case "modal-github":
+        //         return <ModalGithub personGithub={personGithub} openModal={openModal} setOpenModal={setOpenModal}/>;
+        //         break;
+        // }
+
+        /***************************** HEY MAKE SURE TO CHANGE THE THING SO THAT IF WE CLICK THE OVERALL BLACK MODAL BUTTON, IT WILL CLOSE ALL MODALS */
+
+
+        console.log(e.target.id);
+
         setOpenModal(prevValue => !prevValue);
     }
+    // Here we're going to change this openModal thing to handle ALL of the modal clicks and then take over which one with the onclick function
 
 
     const menuIcons = [
-        [<AiFillGithub className="icon-font-size"/>, "#3E84E6"], //Github
-        [<IoFastFoodOutline className="icon-font-size"/>, "#15AB88"], //Favorite Food
-        [<MdDescription className="icon-font-size"/>, "#EB5089"], //Hobbies
-        [<GoLocation className="icon-font-size"/>, "#AFD91A"], //Current_Location
-        [<AiOutlineNumber className="icon-font-size"/>, "#F27127"], //Age
-        [<AiFillStar className="icon-font-size"/>, "#07C7F2"], //Rating
-        [<FaQuestionCircle className="icon-font-size"/>, "#9768D1"], //Interesting Fact
-        [<FaSpotify className="icon-font-size"/>, "#F2B705"] //Spotify
+        ["modal-github", <AiFillGithub className="icon-font-size"/>, "#3E84E6"], //Github
+        ["modal-food", <IoFastFoodOutline className="icon-font-size"/>, "#F2B705"], //Favorite Food
+        ["modal-hobbies", <MdDescription className="icon-font-size"/>, "#EB5089"], //Hobbies
+        ["modal-location", <GoLocation className="icon-font-size"/>, "#AFD91A"], //Current_Location
+        ["modal-age", <AiOutlineNumber className="icon-font-size"/>, "#F27127"], //Age
+        ["modal-rating", <AiFillStar className="icon-font-size"/>, "#07C7F2"], //Rating
+        ["modal-fact", <FaQuestionCircle className="icon-font-size"/>, "#9768D1"], //Interesting Fact
+        ["modal-spotify", <FaSpotify className="icon-font-size"/>, "#15AB88"] //Spotify"#3E84E6"
     ];
     
     return (
         <>
-        {openModal && <ModalGithub openModal={openModal} setOpenModal={setOpenModal}/>}
+        {/* Github */}
+        {openModal && <ModalGithub personGithub={personGithub} openModal={openModal} setOpenModal={setOpenModal}/>}
+        {/* Food */}
+        {openModal && <ModalFood personFood={personFood} openModal={openModal} setOpenModal={setOpenModal}/>}
+
+
         <div className="page-container">
             <div className="circular-menu">
                 <div className="menu-button" onClick={handleSetIsOpen}>
                     {isOpen ? <CohortCloseCircleItem/> : <CohortOpenCircleItem/>}
                 </div>
                 {
-                    menuIcons.map(([icon, color], index) => (
+                    menuIcons.map(([id, icon, color], index) => (
                         <CohortInfoCircleItem
+                            id={id}
                             key={index}
                             openModal={openModal}
                             handleSetOpenModal={handleSetOpenModal}
