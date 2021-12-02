127763ca90b58f75f5ce5e8a317f65397bfbde15
Eric Yi <erickanginyi@gmail.com>
2021-12-02 05:10:53 -0500

    Rename files

---
+++ client/src/Components/Charts.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../Styling/Charts.css"
+
+function Charts() {
+    return (
+        <>
+            <h1>what the hail</h1>
+        </>
+    );
+}
+
+export default Charts;
