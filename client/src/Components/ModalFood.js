2b8192d7fa9b79f96cfd7cc37f11f3f46a041873
Eric Yi <erickanginyi@gmail.com>
2021-12-02 15:30:01 -0500

    Create base code

---
+++ client/src/Components/ModalFood.js
@@ -0,0 +1,35 @@
+import React, {useEffect, useCallback} from "react";
+import "../Styling/Modal.css"
+
+function ModalFood({personFood, openFood, setOpenFood}) {
+    const escPress = useCallback(e => {
+        if (e.key === "Escape" && openFood) {
+            setOpenFood(false);
+        }
+    }, [openFood, setOpenFood]);
+
+    useEffect(() => {
+        document.addEventListener("keydown", escPress);
+        return () => document.removeEventListener("keydown", escPress);
+    }, [escPress])
+
+    // Create a function that makes it so the thing is separates based on comma, render into list
+
+    return (
+        <>
+            {openFood ?
+                <div className="modals">
+                    <div className="modals-box">
+                        <div className="modals-content">
+                            <h2>Favorite Food</h2>
+                            <p>{personFood}</p>
+                            <button onClick={() => setOpenFood(false)}>X</button>
+                        </div>
+                    </div>
+                </div>
+            : null}
+        </>
+    );
+}
+
+export default ModalFood;
