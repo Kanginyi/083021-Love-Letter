b966241d88d8e365b5ecf6b17d552f66f26ad70a
Eric Yi <erickanginyi@gmail.com>
2021-12-02 05:29:14 -0500

    parse through data for charts

--- client/src/Components/Charts.js
+++ client/src/Components/Charts.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import "../Styling/Charts.css"
 
-function Charts() {
+function Charts({personInfo}) {
+    // where originially from, age, flatiron rating, currently live, hobbies, food
+    console.log(personInfo);
+
+    const ageInformation = personInfo?.map(person => person.age);
+    const ratingInformation = personInfo?.map(person => person.flatiron_rating);
+    const locationInformation = personInfo?.map(person => person.current_location);
+
+    // const renderPerson = personInfo?.map(person => {
+    //     return <CohortPerson key={person.id} person={person} handleImageClick={handleImageClick}/>
+    // });
+
     return (
         <>
             <h1>what the hail</h1>
