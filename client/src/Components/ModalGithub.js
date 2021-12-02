1fa3f9442683eae6b1626dd2e55e96c8578a097f
Eric Yi <erickanginyi@gmail.com>
2021-12-02 05:09:56 -0500

    Create Github Modal

--- client/src/Components/ModalGithub.js
+++ client/src/Components/ModalGithub.js
@@ -1,49 +1,37 @@
 import React, {useEffect, useCallback} from "react";
 import "../Styling/Modal.css"
 
-function ModalGithub({openModal, setOpenModal}) {
+function ModalGithub({personGithub, openGithub, setOpenGithub}) {
     const escPress = useCallback(e => {
-        if (e.key === "Escape" && openModal) {
-            setOpenModal(false);
+        if (e.key === "Escape" && openGithub) {
+            setOpenGithub(false);
         }
-    }, [openModal, setOpenModal]);
+    }, [openGithub, setOpenGithub]);
 
     useEffect(() => {
         document.addEventListener("keydown", escPress);
         return () => document.removeEventListener("keydown", escPress);
     }, [escPress])
 
+    // console.log(personSpotify);
+    // Get this shit to render as the spotify link later
+    // const spotifyIFrames = personSpotify?.replace(/^\'<iframe[\s\S]+iframe>\'/gi, "");
+    
+
     return (
         <>
-            {openModal ?
+            {openGithub ?
                 <div className="modals">
                     <div className="modals-box">
                         <div className="modals-content">
                             <h2>GitHub Link</h2>
-                            <p><a href="http://github.com" target="_blank">Github</a></p>
-                            <button onClick={() => setOpenModal(false)}>X</button>
+                            <p><a href={personGithub} target="_blank">{personGithub}</a></p>
+                            <button onClick={() => setOpenGithub(false)}>X</button>
                         </div>
                     </div>
                 </div>
             : null}
-
         </>
-
-        // <div className="modal-background">
-
-        //     <div className="modal-container">
-        //         <button onClick={() => setOpenModal(false)}> &times; </button>
-        //         <div class="modal-title">
-        //             <h1>GitHub Link:</h1>
-        //         </div>
-        //         <div class="modal-body">
-        //             <a href="http://github.com" target="_blank">Github</a>
-        //         </div>
-        //         <div class="modal-footer">
-        //             <p>Bingus Amingus</p>
-        //         </div>
-        //     </div>
-        // </div>
     );
 }
 
