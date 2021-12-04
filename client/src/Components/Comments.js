f425f252cb275d5b891898e6c7bf0d6c945cfb2a
Eric Yi <erickanginyi@gmail.com>
2021-12-03 19:42:25 -0500

    Import code

--- client/src/Components/Comments.js
+++ client/src/Components/Comments.js
@@ -21,6 +21,96 @@ function Comments({personInfo}) {
     return (
         <>
             <h1>Comments</h1>
+            <form>
+                <section>
+                <div className="container">
+                    <h2>New Comment</h2>
+                    <div className="row100">
+                        <div className="col">
+                            <div className="inputBox">
+                            <input
+                                type="number"
+                                name="amount"
+                                required="required"
+                                // value={newContract.amount}
+                                // onChange={handleChange}
+                            />
+                            <span className="text">Amount</span>
+                            <span className="line"></span>
+                            </div>
+                        </div>
+
+                        <div className="col">
+                            <div className="inputBox">
+                            <input
+                            type="text"
+                            name="collateral"
+                            required="required"
+                            // value={newContract.collateral}
+                            // onChange={handleChange}
+                            />
+                            <span className="text">Collateral</span>
+                            <span className="line"></span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="row100">
+                        <div className="col">
+                            <div className="inputBox">
+                            <input
+                            type="number"
+                            name="duration"
+                            required="required"
+                            // value={newContract.duration}
+                            // onChange={handleChange}
+                            />
+                            <span className="text">Duration</span>
+                            <span className="line"></span>
+                            </div>
+                        </div>
+
+                        <div className="col">
+                            <div className="inputBox">
+                            {/* <select onChange={handleSelect}> */}
+                            <select>
+                            <option value="none">Select a Lender</option>
+                            <option value="1">Section A</option>
+                            <option value="2">Section B</option>
+                            <option value="3">Capital Two</option>
+                            <option value="4">Silverman Sucks</option>
+                            <option value="5">TT Bank</option>
+                            </select>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="row100">
+                        <div className="col">
+                            <div className="inputBox textarea">
+                                <textarea
+                                    style={{resize: "none"}}
+                                    required="required"
+                                    // onChange={handleChange}
+                                    name="description"
+                                    // value={newContract.description}
+                                >
+                                </textarea>
+                                <span className="text">Description</span>
+                                <span className="line"></span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="row100">
+                    <div id="new-contract-button" className="col">
+                        <button type="submit">Post a Comment</button>
+                    </div>
+                    </div>
+
+                </div>
+                </section>
+            </form>
         </>
     );
 }
