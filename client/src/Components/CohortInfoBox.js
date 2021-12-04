4cb17cab844976bb75307be96c95b52676c9cffc
Eric Yi <erickanginyi@gmail.com>
2021-12-04 03:06:33 -0500

    Remove comments

--- client/src/Components/CohortInfoBox.js
+++ client/src/Components/CohortInfoBox.js
@@ -1,19 +1,8 @@
-import React, {useState} from 'react';
+import React from 'react';
 import "../Styling/CohortInfoBox.css"
-
-import {FaRegSmileBeam} from "react-icons/fa"
 
 function CohortInfoBox({personInfo, clickedName}) {
     const choosePerson = personInfo?.find(person => person.name === clickedName)
-
-
-    // HEY HEY UPDATE THE THING SO THAT THERE'S ACTUALLY SOMETHING THAT'S THERE ON PAGE LOAD
-
-    // Bing usestate inside of this shit and then set it equal to a empty string
-    // if it's that, conditional render each of the component things inside the card
-    // set the initial states to the empy things
-
-    // Ribbon thing on youtube
 
     const flipCard = () => {
         const card = document.querySelector(".card-inner");
@@ -23,23 +12,6 @@ function CohortInfoBox({personInfo, clickedName}) {
     // Add a thing where it'll erase the front part of the card when you send it to the back
 
     return (
-        // <div className="card">
-        //     <div className="card-header">{clickedName !== "" ? choosePerson?.name : "Click an Image"}</div>
-        //     <div className="card-body">
-        //         <img
-        //             className="card-image"
-        //             src={clickedName !== "" ? choosePerson?.picture : "https://i.imgur.com/gaxXG7T.jpg"}
-        //             alt={choosePerson?.name}
-        //             title={choosePerson?.name}
-        //             width="100%"
-        //         /> <br/>
-        //         {clickedName !== "" ? choosePerson?.description : "Please?"}
-        //     </div>
-        //     <div className="card-footer">
-        //         From: {clickedName !== "" ? choosePerson?.location_from : "???"}
-        //     </div>
-        // </div>
-
         <div className="card" onClick={flipCard}>
             <div className="card-inner">
                 <div className="card-face card-face-front">
@@ -75,8 +47,3 @@ function CohortInfoBox({personInfo, clickedName}) {
 }
 
 export default CohortInfoBox;
-
-//Load these inside of the fill in bbox on the side
-
-// description: "An idiot trying to learn how to not be an idiot."
-// location_from: "New York City, New York"
