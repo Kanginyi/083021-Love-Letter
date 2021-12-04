d0f3d85264d4b27f5b5182824b5899f3cb8fce7b
Eric Yi <erickanginyi@gmail.com>
2021-12-04 03:37:06 -0500

    Create file and create modal and logic

---
+++ client/src/Components/ModalHobbies.js
@@ -0,0 +1,44 @@
+import React, {useEffect, useCallback} from "react";
+import "../Styling/CohortModals.css"
+
+function ModalHobbies({personHobbies, openModal, setOpenModal}) {
+    const escPress = useCallback(e => {
+        if (e.key === "Escape" && openModal) {
+            setOpenModal(false);
+        }
+    }, [openModal, setOpenModal]);
+
+    useEffect(() => {
+        document.addEventListener("keydown", escPress);
+        return () => document.removeEventListener("keydown", escPress);
+    }, [escPress])
+
+    // Create a function that separates each food into an li for the list
+    const hobbiesArray = personHobbies?.split(", ");
+
+    const listHobbies = hobbiesArray?.map(hobby => {
+        return <li key={hobby}>{hobby}</li>
+    })
+
+    console.log(listHobbies);
+
+    return (
+        <>
+            {openModal ?
+                <div className="modals">
+                    <div id="modals-hobbies" className="modals-box">
+                        <div className="modals-content">
+                            <h2>Hobbies</h2>
+                            <ul>
+                                {listHobbies}
+                            </ul>
+                            <button onClick={() => setOpenModal(false)}>X</button>
+                        </div>
+                    </div>
+                </div>
+            : null}
+        </>
+    );
+}
+
+export default ModalHobbies;    
