ce185bd4e70236cb2e35f9b8da75e151b9f595e1
Eric Yi <erickanginyi@gmail.com>
2021-12-05 02:41:42 -0500

    Add user_id, likes, and dislikes to table

--- db/migrate/20211205010102_create_comments.rb
+++ db/migrate/20211205010102_create_comments.rb
@@ -1,9 +1,13 @@
 class CreateComments < ActiveRecord::Migration[6.1]
   def change
     create_table :comments do |t|
+      t.integer :user_id
       t.string :first_name
       t.string :last_name
       t.text :comment
+      t.integer :likes
+      t.integer :dislikes
+    
 
       t.timestamps
     end
