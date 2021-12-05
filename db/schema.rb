2b38fb277a3efcca4d6583993661174d01fc24af
Eric Yi <erickanginyi@gmail.com>
2021-12-04 20:28:55 -0500

    Migrate new table data

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_03_045427) do
+ActiveRecord::Schema.define(version: 2021_12_05_010102) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.text "comment"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "instructors", force: :cascade do |t|
     t.string "name"
