83f467799aafdbb238b42d785b3e65b694f0bcf0
Eric Yi <erickanginyi@gmail.com>
2021-12-05 13:59:23 -0500

    Add likes and dislike buttons

--- client/src/Components/CommentsCard.js
+++ client/src/Components/CommentsCard.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import "../Styling/CommentsCard.css";
 
-function CommentsCard({firstName, lastName, comment, time}) {
+function CommentsCard({firstName, lastName, comment, likes, dislikes, time}) {
     return (
         <article className="comments-card">
             <header className="comments-card-header">
                 <h2>{firstName} {lastName}</h2>
                 <p>{comment}</p>
                 <p>{time}</p>
+                <button>{likes} Likes</button>
+                <button>{dislikes} Dislikes</button>
             </header>
         </article>
     );
