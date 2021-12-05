cce2a198aad599999604a75a1b00604104a0d539
Eric Yi <erickanginyi@gmail.com>
2021-12-04 22:09:04 -0500

    Create a form

--- client/src/Components/Comments.js
+++ client/src/Components/Comments.js
@@ -1,40 +1,123 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import CommentsCard from './CommentsCard';
 import "../Styling/Comments.css";
 
 
-function Comments({personInfo}) {
-    // where originially from, age, flatiron rating, currently live, hobbies, food
-    console.log(personInfo);
+function Comments() {
+    // Fetch comments information
+    const [commentData, setCommentData] = useState([]);
 
-    const ageInformation = personInfo?.map(person => person.age);
-    const ratingInformation = personInfo?.map(person => person.flatiron_rating);
-    const locationInformation = personInfo?.map(person => person.current_location);
+    useEffect(() => {
+        fetch("/comments")
+            .then(resp => resp.json())
+            .then(data => setCommentData(data))
+    }, []);
 
-    // const renderPerson = personInfo?.map(person => {
-    //     return <CohortPerson key={person.id} person={person} handleImageClick={handleImageClick}/>
-    // });
+    const [newComment, setNewComment] = useState({
+        first_name: "",
+        last_name: "",
+        comment: ""
+    });
 
-    // // Unique ages array
-    // const ages = ageInformation?.filter((value, index, self) => {
-    //     return self.indexOf(value) === index;
-    // });
+    const handleChange = e => {
+        setNewComment({...newComment, [e.target.name]: e.target.value});
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        fetch("/comments", {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(newComment)
+        })
+            .then(resp => resp.json())
+            .then(data => setCommentData([...commentData, data]))
+        
+        setNewComment({
+            first_name: "", last_name: "", comment: ""
+        });
+    }
 
     return (
         <>
+
+        {/* Submission Form */}
+        <div>
+            <form onSubmit={handleSubmit}>
+            <section>
+
+            <div className="container">
+                <h2>Add a Comment!</h2>
+                <div className="row100">
+                    <div className="col">
+                        <div className="inputBox">
+                        <input
+                            type="text"
+                            name="first_name"
+                            required="required"
+                            value={newComment.first_name}
+                            onChange={handleChange}
+                        />
+                        <span className="text">First Name</span>
+                        <span className="line"></span>
+                        </div>
+                    </div>
+
+                    <div className="col">
+                        <div className="inputBox">
+                        <input
+                        type="text"
+                        name="last_name"
+                        required="required"
+                        value={newComment.last_name}
+                        onChange={handleChange}
+                        />
+                        <span className="text">Last Name</span>
+                        <span className="line"></span>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="row100">
+                    <div className="col">
+                        <div className="inputBox textarea">
+                            <textarea
+                                style={{resize: "none"}}
+                                required="required"
+                                onChange={(handleChange)}
+                                name="comment"
+                                value={newComment.comment}
+                            ></textarea>
+                            <span className="text">Comment</span>
+                            <span className="line"></span>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="row100">
+                    <div id="new-contract-button" className="col">
+                        <button type="submit">Post Your Comment</button>
+                    </div>
+                </div>
+
+            </div>
+            </section>
+            </form>
+            </div>
+
+            {/* Where the comments are rendered */}
             <section className="comment-list">
-                <CommentsCard />
-                <CommentsCard />
-                <CommentsCard />
-                <CommentsCard />
-                <CommentsCard />
-                <CommentsCard />
-                <CommentsCard />
-                <CommentsCard />
-                <CommentsCard />
-                <CommentsCard />
-                <CommentsCard />
-                <CommentsCard />
+                {
+                    commentData?.map(comment => {
+                        return <CommentsCard
+                            firstName={comment.first_name}
+                            lastName={comment.last_name}
+                            comment={comment.comment}
+                            time={comment.created_at}
+                        />
+                    })
+                }
             </section>
         </>
     );
