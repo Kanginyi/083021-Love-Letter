87b12f28bb66d264b8a34db766f8d6ea9f08da9d
Eric Yi <erickanginyi@gmail.com>
2021-12-04 19:46:16 -0500

    Enter dummy text

---
+++ client/src/Components/CommentsCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "../Styling/CommentsCard.css";
+
+function CommentsCard() {
+    return (
+        <article className="comments-card">
+            <header className="comments-card-header">
+                <h2>Bingus Amingus</h2>
+                <p>Hey y'all, hope everything's goes well. I need some mental assistance but other than that, things are going well WOO.</p>
+            </header>
+        </article>
+    );
+}
+
+export default CommentsCard;
