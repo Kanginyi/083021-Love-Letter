16e47ac3947c7e076858cecd057ed626e2d92d16
Eric Yi <erickanginyi@gmail.com>
2021-12-04 22:11:09 -0500

    Add time, will be changing that formatting later

--- client/src/Components/CommentsCard.js
+++ client/src/Components/CommentsCard.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import "../Styling/CommentsCard.css";
 
-function CommentsCard() {
+function CommentsCard({firstName, lastName, comment, time}) {
     return (
         <article className="comments-card">
             <header className="comments-card-header">
-                <h2>Bingus Amingus</h2>
-                <p>Hey y'all, hope everything's goes well. I need some mental assistance but other than that, things are going well WOO.</p>
+                <h2>{firstName} {lastName}</h2>
+                <p>{comment}</p>
+                <p>{time}</p>
             </header>
         </article>
     );
