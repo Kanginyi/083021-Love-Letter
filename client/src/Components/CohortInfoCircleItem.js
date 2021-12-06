294d35b430f52d889bd5a7ad4f2c64d57afc81ee
Eric Yi <erickanginyi@gmail.com>
2021-12-06 11:03:13 -0500

    Remove commented code'

--- client/src/Components/CohortInfoCircleItem.js
+++ client/src/Components/CohortInfoCircleItem.js
@@ -20,17 +20,3 @@ function CohortInfoCircleItem({handleSetOpenModal, openModal, id, icon, color, r
 }
 
 export default CohortInfoCircleItem;
-
-// age: 26
-// current_location: "New York"
-// description: "An idiot trying to learn how to not be an idiot."
-// favorite_food: "Sushi, Korean BBQ, Noodles"
-// flatiron_rating: 9
-// github: "https://github.com/Kanginyi"
-// hobbies: "Video games, reading, walking around with my cat, "
-// id: 5
-// instructor: {id: 1, name: 'Dickhead McGee'}
-// interesting_fact: "I have a polydactyl cat named Henri who has 7 fingers on both of his front two paws!"
-// location_from: "New York City, New York"
-// picture: "https://i.imgur.com/T3Dnjf6.jpg"
-// song: "<iframe src=\"https://open.spotify.com/embed/track/51Ful1v7b81FchIFFTAQ05
