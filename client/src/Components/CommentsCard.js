9bd5e2e910c4732ebdd5b7091c5cf7962161eff7
Eric Yi <erickanginyi@gmail.com>
2021-12-06 05:18:10 -0500

    Move buttons into separate div to test

--- client/src/Components/CommentsCard.js
+++ client/src/Components/CommentsCard.js
@@ -4,13 +4,18 @@ import "../Styling/CommentsCard.css";
 function CommentsCard({firstName, lastName, comment, likes, dislikes, time}) {
     return (
         <article className="comments-card">
-            <header className="comments-card-header">
-                <h2>{firstName} {lastName}</h2>
-                <p>{comment}</p>
-                <p>{time}</p>
+            <header>
+                <div className="comments-card-header">
+                    <h2>{firstName} {lastName}</h2>
+                    <p>{comment}</p>
+                </div>
+                
                 <div className="comments-card-button-container">
                     <button>{likes} Likes</button>
                     <button>{dislikes} Dislikes</button>
+                </div>
+                <div>
+                    <p>{time}</p>
                 </div>
             </header>
         </article>
