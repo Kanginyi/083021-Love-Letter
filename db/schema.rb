8b2e493da9c106cc2198b2e721b6633a47ecf83b
Eric Yi <erickanginyi@gmail.com>
2021-12-07 06:41:11 -0500

    Migrate new unfort table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_05_010102) do
+ActiveRecord::Schema.define(version: 2021_12_07_032627) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,13 @@ ActiveRecord::Schema.define(version: 2021_12_05_010102) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["instructor_id"], name: "index_students_on_instructor_id"
+  end
+
+  create_table "unfortunates", force: :cascade do |t|
+    t.string "name"
+    t.string "picture"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "students", "instructors"
