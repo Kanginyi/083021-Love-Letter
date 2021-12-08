fc6d472b91e08e42edad5c35e6319d6d387d3204
Eric Yi <erickanginyi@gmail.com>
2021-12-08 14:20:13 -0500

    Add optional chaining to renderings, remove unused code

--- client/src/Components/CohortPerson.js
+++ client/src/Components/CohortPerson.js
@@ -8,9 +8,9 @@ function CohortPerson({person, handleImageClick}) {
         <img
             className="carousel-image-box"
             onClick={(e) => handleImageClick(e.target.alt)}
-            src={person.picture}
-            alt={person.name}
-            title={person.name}
+            src={person?.picture}
+            alt={person?.name}
+            title={person?.name}
             width="150px"
             height="150px"
         />
@@ -20,18 +20,3 @@ function CohortPerson({person, handleImageClick}) {
 }
 
 export default CohortPerson;
-
-
-// age: 26
-// current_location: "New York"
-// description: "An idiot trying to learn how to not be an idiot."
-// favorite_food: "Sushi, Korean BBQ, Noodles"
-// flatiron_rating: 9
-// github: "https://github.com/Kanginyi"
-// hobbies: "Video games, reading, walking around with my cat, "
-// id: 5
-// instructor: {id: 1, name: 'Dickhead McGee'}
-// interesting_fact: "I have a polydactyl cat named Henri who has 7 fingers on both of his front two paws!"
-// location_from: "New York City, New York"
-// picture: "https://i.imgur.com/T3Dnjf6.jpg"
-// song: "<iframe src=\"https://open.spotify.com/embed/track/51Ful1v7b81FchIFFTAQ05
