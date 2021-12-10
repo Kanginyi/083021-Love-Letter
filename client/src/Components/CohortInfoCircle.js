34f7ef86f10bfe052523be84601c5935e97f4d41
Eric Yi <erickanginyi@gmail.com>
2021-12-10 02:54:50 -0500

    Remove comments

--- client/src/Components/CohortInfoCircle.js
+++ client/src/Components/CohortInfoCircle.js
@@ -4,7 +4,6 @@ import CohortOpenCircleItem from './CohortOpenCircleItem';
 import CohortCloseCircleItem from './CohortCloseCircleItem';
 import "../Styling/CohortInfoCircle.css"
 
-// *THIS IS A TEST, DOUNLE CHECK THIS SHIT LATER
 import ModalGithub from './ModalGithub';
 import ModalFood from './ModalFood';
 import ModalHobbies from './ModalHobbies';
@@ -13,7 +12,6 @@ import ModalAge from './ModalAge';
 import ModalRating from './ModalRating';
 import ModalFact from './ModalFact';
 import ModalSpotify from './ModalSpotify';
-
 
 import {AiFillGithub, AiOutlineNumber, AiFillStar} from "react-icons/ai";
 import {FaSpotify, FaQuestionCircle} from "react-icons/fa";
@@ -151,17 +149,3 @@ function CohortInfoCircle({personInfo, clickedName}) {
 }
 
 export default CohortInfoCircle;
-
-// age: 26
-// current_location: "New York"
-// description: "An idiot trying to learn how to not be an idiot."
-// favorite_food: "Sushi, Korean BBQ, Noodles"
-// flatiron_rating: 9
-// github: "https://github.com/Kanginyi"
-// hobbies: "Video games, reading, walking around with my cat, "
-// id: 5
-// instructor: {id: 1, name: 'Dickhead McGee'}
-// interesting_fact: "I have a polydactyl cat named Henri who has 7 fingers on both of his front two paws!"
-// location_from: "New York City, New York"
-// picture: "https://i.imgur.com/T3Dnjf6.jpg"
-// song: "<iframe src=\"https://open.spotify.com/embed/track/51Ful1v7b81FchIFFTAQ05
